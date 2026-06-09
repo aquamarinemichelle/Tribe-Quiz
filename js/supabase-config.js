b4d2f35e290ca235f2a466f1162df251bf798f26
Michelle Mazibuko <ntombezinhlemichelle@outlook.com>
2026-06-09 11:38:58 +0200

    gitignore

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,3 +1,3 @@
 
-const SUPABASE_URL      = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_PUBLIC_KEY';
+const SUPABASE_URL      = 'https://mchehyxrtiynjwlvtzlb.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_V8N-Aht5wweqJD3pK6jaYg_At_Qv_Mw';
